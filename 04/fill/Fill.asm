db77aa4e72a4e35cb0c7f659659a66889c5ee9de
vasll <valentinosoll17@gmail.com>
2023-06-08 12:38:12 +0200

    Finished Fill.asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -5,15 +5,13 @@
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
+// 16bit*32=512px (Screen width.  512 Columns)
+// 16bit*16=256px (Screen height. 256 Rows)
+// 512px*256px = 131072px (Screen total pixels)
 
 @16384              // Go to screen address
 D=A
 @screen_pointer     // Store screen base address     
-M=D
-
-@24575              // Screen end address
-D=A
-@screen_end_address // Store screen end address
 M=D
 
 @24576              // Go to keyboard address
@@ -21,6 +19,10 @@ D=A                 // Store address in D register
 @keyboard_pointer   // Store address of keyboard
 M=D
 
+@8192               // Total screen addresses: 24576 - 16384 = 8192
+D=A
+@screen_address_cnt // Keep the count of how many 16bit screen addresses are there
+M=D
 
 
 // keyboard listener loop
@@ -41,19 +43,12 @@ M=D
 
 // Colors the display black and goes back to LOOP
 (FILL)
-    // TODO fill screen logic
-
-    // 16bit*32=512px (Screen width.  512 Columns)
-    // 16bit*16=256px (Screen height. 256 Rows)
-    // 512px*256px = 131072px (Screen total pixels)
-
-    // Fill row
-    @32
-    D=A
+    @screen_address_cnt
+    D=M
     @row_reg_counter    // Set row_reg_counter to 32
     M=D
 
-    (FILL_ROW)
+    (FILL_SCREEN)
         @screen_pointer     // Go to screen_pointer
         A=M                 
         M=-1                // Set all 16 bits of current register to 1
@@ -65,8 +60,14 @@ M=D
         M=M-1
         D=M
 
-        @FILL_ROW           // If (row_reg_counter > 0) GOTO FILL_ROW
+        @FILL_SCREEN           // If (row_reg_counter > 0) GOTO FILL_SCREEN
         D;JGT
+
+    // Reset screen pointer to 16348
+    @16384
+    D=A
+    @screen_pointer
+    M=D
 
     @LOOP               // Jump back to LOOP
     0;JMP
@@ -74,7 +75,32 @@ M=D
 
 // Clear the display and goes back to LOOP
 (CLEAR)
-    // TODO clear screen logic code here
+    @screen_address_cnt
+    D=M
+    @row_reg_counter    // Set row_reg_counter to 32
+    M=D
+
+    (CLEAR_SCREEN)
+        @screen_pointer     // Go to screen_pointer
+        A=M                 
+        M=0                // Set all 16 bits of current register to 1
+
+        @screen_pointer     // Set pointer to next 16bit register
+        M=M+1
+
+        @row_reg_counter    // Decrement row_reg_counter by 1
+        M=M-1
+        D=M
+
+        @CLEAR_SCREEN           // If (row_reg_counter > 0) GOTO CLEAR_SCREEN
+        D;JGT
+
+    // Reset screen pointer to 16348
+    @16384
+    D=A
+    @screen_pointer
+    M=D
+
     @LOOP               // Jump back to LOOP
     0;JMP
 
